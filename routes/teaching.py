0340162a53bb17be7c948bc5951c4da46ca3e7ea
Talha_Altair <cooltalha27@gmail.com>
2023-04-24 01:26:17 +0530

    Updates for 2023-04-24 01:26:17

---
+++ routes/teaching.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify, request
+import pandas as pd
+teaching = Blueprint('teaching', __name__)
+
+
+@teaching.route('/passpercentage', methods=['GET', 'POST'])
+def passpercentage():
+
+    pass_data = pd.DataFrame({
+    'Subject': ['Compiler', 'Green Design', 'SPM', 'Operating System', 'Cloud Computing'],
+    'Pass Percentage': [76, 99, 97, 86, 82],
+    'year': [1,2,2,3,3],
+    'Subject Code': ['CS 201', 'CS 301', 'CS 401', 'CS 501', 'CS 601'],
+    })
+
+    return jsonify(pass_data.to_dict(orient='records'))
