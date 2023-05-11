04af7c6cc082a4971753da052cc11a495750ad32
Talha-Altair <cooltalha27@gmail.com>
2023-05-11 10:36:33 +0530

    Updates for 2023-05-11 10:36:33

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ from routes.teaching import teaching
 from routes.upload import uploader
 from routes.login import login
 from routes.signup import signup
+from routes.dashboard import dashboard
 
 app.register_blueprint(teaching, url_prefix='/teaching')
 app.register_blueprint(uploader, url_prefix='/upload')
 app.register_blueprint(login, url_prefix='/login')
 app.register_blueprint(signup, url_prefix='/signup')
+app.register_blueprint(dashboard, url_prefix='/dashboard')
 
 @app.route('/ping')
 def index():
